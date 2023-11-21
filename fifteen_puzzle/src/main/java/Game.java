cbd0a60a197db5de83c0b11cff314b2858345e1c
kimjooyoung <kjy915875@gmail.com>
2023-11-21 14:36:38 +0900

    feat. Complete Game class

--- fifteen_puzzle/src/main/java/Game.java
+++ fifteen_puzzle/src/main/java/Game.java
@@ -5,18 +5,40 @@ import java.util.*;
 public class Game {
 
     private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private final Puzzle puzzle = new Puzzle();
 
-    public void doGame() {
+    public Game() {
+    }
+
+    public void doGame() throws IOException, NumberFormatException{
         int turn = 1;
-
+        while (!puzzle.checkIsSorted()) {
+            printTurnAndPuzzle(turn);
+            List<Integer> input = userInput();
+            puzzle.change(input.get(0), input.get(1));
+            turn++;
+        }
+        printTurnAndPuzzle(turn);
+        System.out.println("축하합니다! " + turn + "턴만에 퍼즐을 완성하셨습니다!");
     }
 
     private void printTurnAndPuzzle(int turn) {
         StringBuilder sb = new StringBuilder();
-        sb.append("Turn ");
+        sb.append("\nTurn ");
         sb.append(turn);
         sb.append("\n");
+        addPuzzleNums(sb);
+        System.out.println(sb.toString());
+    }
 
+    private void addPuzzleNums(StringBuilder sb) {
+        sb.append("[");
+        for (Integer num : puzzle.getPuzzle()) {
+            sb.append(num);
+            sb.append(", ");
+        }
+        sb.delete(sb.length() - 2, sb.length() - 1);
+        sb.append("]");
     }
 
     private List<Integer> userInput() throws IOException, NumberFormatException {
@@ -29,7 +51,7 @@ public class Game {
                 result = parse(input);
                 correctInput = true;
             } catch (NumberFormatException numberFormatException) {
-                System.out.println("잘못 입력하셨습니다. 다시 입력해 주세요.");
+                System.out.println("\n잘못 입력하셨습니다. 다시 입력해 주세요.\n");
             }
         }
         return result;
---
+++ fifteen_puzzle/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        Game game = new Game();
+        game.doGame();
+    }
+}
