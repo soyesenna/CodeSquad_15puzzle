9711203cac0e1c6a03d1b951bae8f8b251faf7b7
kimjooyoung <kjy915875@gmail.com>
2023-11-21 16:51:05 +0900

    feat. Make GameV2.userInput and checkCorrectInput method

--- fifteen_puzzle/src/main/java/GameV2.java
+++ fifteen_puzzle/src/main/java/GameV2.java
@@ -1,9 +1,12 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.io.*;
 
 public class GameV2 implements Game{
 
-
+    private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private final Puzzle puzzle = new PuzzleV2();
 
     @Override
     public void doGame() throws IOException {
@@ -12,7 +15,31 @@ public class GameV2 implements Game{
 
     @Override
     public List<Integer> userInput() throws IOException, NumberFormatException {
-        return null;
+        boolean correctInput = false;
+        String input;
+        int result = 0;
+        while (!correctInput) {
+            System.out.print("숫자 입력> ");
+            input = br.readLine();
+            try {
+                result = checkCorrectInput(input);
+                correctInput = true;
+            } catch (NumberFormatException numberFormatException) {
+                System.out.println("\n잘못 입력하셨습니다. 다시 입력해 주세요.\n");
+            }
+        }
+        return new ArrayList<>(List.of(result));
+    }
+
+    private int checkCorrectInput(String str) {
+        int num = 0;
+        try {
+            num = Integer.parseInt(str);
+            if (num < 1 || num > 15) throw new NumberFormatException();
+        } catch (NumberFormatException numberFormatException) {
+            throw new NumberFormatException();
+        }
+        return num;
     }
 
     @Override
